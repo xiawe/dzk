50a62fe9448a624ba4b3d63273fc93e2bff491ef
xiawe <749812994@qq.com>
2018-11-26 07:35:26 +0800

    img

--- dzk-3/game.js
+++ dzk-3/game.js
@@ -38,23 +38,27 @@ var Game = function() {
             runLoop()
         }, 1000 / fps)
     }
-    setTimeout(function() {
-        runLoop()
-    }, 1000 / fps)
-    // setInterval(function() {
-    //     log('fps', window.fps)
-    //     var actions = Object.keys(g.actions)
-    //     for (var i = 0; i < actions.length; i++) {
-    //         var key = actions[i]
-    //         if (g.keydowns[key]) {
-    //             g.actions[key]()
-    //         }            
-    //     }
-    //     g.update()
-    //     // clear
-    //     context.clearRect(0, 0, canvas.width, canvas.height)
-    //     // draw
-    //     g.draw()
-    // }, 1000 / window.fps)
+    // 载入所有图片后，再执行
+    var names = Object.keys(images)
+    var loads = []
+    for (var i = 0; i < names.length; i++) {
+        if (images[name]) {
+            loads.push(1)
+        }
+
+    }
+    g.run = function() {
+        setTimeout(function() {
+            runLoop()
+        }, 1000 / fps)
+    }
     return g
 }
+    console.log('very outside', b)
+    for (var i = 0; i < 5; i++) {
+        var b = i
+        console.log('outside', b)     
+        setTimeout(function() {
+            console.log('inside',b)
+        }, 2000)   
+    }
